3db751c90796322d9ad087dc5a1dc7e59edc4c06
prazerjao <joaoalves1638@gmail.com>
2021-08-30 19:13:54 -0300

    First Version of Front End

---
+++ Nano5/Componentes/Celulas/CheckListsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CheckListsTableViewCell.swift
+//  Nano5
+//
+//  Created by João Victor on 30/08/21.
+//
+
+import UIKit
+
+class CheckListsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
